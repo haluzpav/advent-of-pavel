62df42ce3d5e227a15c3a7848d8fac89508f4785
haluzpav <24211726+haluzpav@users.noreply.github.com>
2022-12-06 08:59:03 +0100

    Day 6

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,19 @@
+class Day06(filename: String) {
+    private val input: List<String> = readInput(filename)
+
+    fun part1(): List<Int> = findRepetitions(4)
+
+    fun part2(): List<Int> = findRepetitions(14)
+
+    private fun findRepetitions(count: Int): List<Int> = input.map { s ->
+        (0..s.length - count)
+            .first { s.substring(it until it + count).toSet().size == count }
+            .let { it + count }
+    }
+}
+
+fun main() {
+    val task = Day06("Day06")
+    println(task.part1())
+    println(task.part2())
+}
