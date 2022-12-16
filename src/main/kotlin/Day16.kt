b47777264a9d713671ff6ec2b558a812abaa49c8
haluzpav <24211726+haluzpav@users.noreply.github.com>
2022-12-16 15:57:05 +0100

    Day 16, part 1 [works only on test data 😭]

---
+++ src/main/kotlin/Day16.kt
@@ -0,0 +1,73 @@
+class Day16(inputName: String) {
+    private val input: Sequence<String> = readInput(inputName)
+
+    private val inputRegex = Regex("""^Valve ([A-Z]+) has flow rate=([0-9]+); tunnels? leads? to valves? (.+)$""")
+
+    fun part1(): Int {
+        var valves = parseValves()
+        valves = valves.map { if (it.name == "AA") it.copy(cameFrom = listOf(-1)) else it }
+        val minutes = 5
+        for (minute in 1..minutes) {
+            val newValves = valves.map { valve ->
+                val (maxIncoming, maxIncomingFrom) = valve.leadsTo.fold(Int.MIN_VALUE to null as Int?) { (max, i), vi ->
+                    val v = valves[vi]
+                    if (v.cameFrom.isNotEmpty() && v.released > max) v.released to vi else max to i
+                }
+                val canOpenSelf = valve.cameFrom.isNotEmpty() && valve.i !in valve.opened && valve.flow > 0
+                val selfReleasable = if (canOpenSelf) (minutes - minute) * valve.flow else 0
+                val releasedAfterSelfOpen = valve.released + selfReleasable
+                val exploredByIncoming = valve.cameFrom.isEmpty() && maxIncomingFrom != null
+                val overridenByIncoming = maxIncoming > releasedAfterSelfOpen
+                when {
+                    exploredByIncoming || overridenByIncoming -> valve.copy(
+                        cameFrom = valves[maxIncomingFrom!!].cameFrom + maxIncomingFrom,
+                        opened = valves[maxIncomingFrom].opened,
+                        released = maxIncoming,
+                    )
+                    canOpenSelf -> valve.copy(
+                        opened = valve.opened + valve.i,
+                        released = releasedAfterSelfOpen,
+                    )
+                    else -> valve
+                }
+            }
+            valves = newValves
+        }
+        return valves.maxOf { it.released }
+    }
+
+    fun part2(): Int = -1
+
+    private fun parseValves(): List<Valve> = input.mapIndexed { index, line ->
+        val (name, flow, leadsTos) = inputRegex.matchEntire(line)!!.destructured
+        Valve(
+            i = index,
+            name = name,
+            flow = flow.toInt(),
+        ) to leadsTos.split(", ")
+    }.toMap().let { valvesToLeadsToNames ->
+        valvesToLeadsToNames.keys.map { valve ->
+            valve.copy(
+                leadsTo = valvesToLeadsToNames[valve]!!.map { leadsToName ->
+                    valvesToLeadsToNames.keys.indexOfFirst { it.name == leadsToName }
+                },
+            )
+        }
+    }
+
+    private data class Valve(
+        val i: Int,
+        val name: String,
+        val flow: Int,
+        val leadsTo: List<Int> = emptyList(),
+        val cameFrom: List<Int> = emptyList(),
+        val opened: List<Int> = emptyList(),
+        val released: Int = 0,
+    )
+}
+
+fun main() {
+    val task = Day16("Day16")
+    println(task.part1())
+    println(task.part2())
+}
---
+++ src/test/kotlin/Day16Test.kt
@@ -0,0 +1,16 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day16Test {
+    private val task = Day16("Day16_test")
+
+    @Test
+    fun testPart1() {
+        assertEquals(1651, task.part1())
+    }
+
+    @Test
+    fun testPart2() {
+        assertEquals(-1, task.part2())
+    }
+}
