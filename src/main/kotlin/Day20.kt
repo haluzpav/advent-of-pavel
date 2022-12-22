d800098a393046102a184a777ca229b025853cd9
haluzpav <24211726+haluzpav@users.noreply.github.com>
2022-12-22 21:16:12 +0100

    Mix ArrayList and LinkedList for part 2 😵‍💫

--- src/main/kotlin/Day20.kt
+++ src/main/kotlin/Day20.kt
@@ -3,57 +3,47 @@ class Day20(inputName: String) {
 
     fun part1(): Long {
         val list = parseList().toList()
-        val mixedList = mix(list)
+        val mixedList = mix(list, rounds = 1)
         return getGroveCoors(mixedList)
     }
 
     fun part2(): Long {
         val list = parseList()
         val decryptedList = list.map { it * 811_589_153 }.toList()
-        val mixedList = (1..10).fold(decryptedList) { acc, _ -> mix(acc) }
+        val mixedList = mix(decryptedList, rounds = 10)
         return getGroveCoors(mixedList)
     }
 
     private fun parseList(): Sequence<Long> = input.map { it.toLong() }
 
-    private fun mix(originalList: List<Long>): List<Long> {
-        val list = originalList.toMutableList()
-        val size = list.size
-        val isMoved = MutableList(size) { false }
+    private fun mix(originalList: List<Long>, rounds: Int): List<Long> {
+        val nodes: MutableList<Node> = Node.listFromLongs(originalList).toMutableList()
+        val firstNode = nodes.first()
+        val size = nodes.size
         val cycleSize = size - 1
 
-        fun removeAt(index: Int) {
-            list.removeAt(index)
-            isMoved.removeAt(index)
-        }
-
-        fun add(index: Int, value: Long) {
-            list.add(index, value)
-            isMoved.add(index, true)
-        }
-
-        var index = 0
-        while (index < size) {
-            val n = list[index]
-            val newPosMaybeNegative = (index + n).rem(cycleSize).toInt()
-            val newPos = (newPosMaybeNegative + cycleSize).rem(cycleSize)
-            when {
-                newPos == index -> {
-                    isMoved[index] = true
+        for (round in 1..rounds) {
+            var node = firstNode
+            while (node.mixedCount < round) {
+                val index = nodes.indexOf(node)
+                val newPosMaybeNegative = (index + node.value).rem(cycleSize).toInt()
+                val newPos = (newPosMaybeNegative + cycleSize).rem(cycleSize)
+                when {
+                    newPos < index -> {
+                        nodes.removeAt(index)
+                        nodes.add(newPos, node)
+                    }
+                    newPos > index -> {
+                        nodes.add(newPos + 1, node)
+                        nodes.removeAt(index)
+                    }
+                    else -> Unit
                 }
-                newPos < index -> {
-                    removeAt(index)
-                    add(newPos, n)
-                }
-                else -> {
-                    add(newPos + 1, n)
-                    removeAt(index)
-                }
+                node.mixedCount++
+                node = node.nextNode
             }
-            while (index < size && isMoved[index]) index++
         }
-        check(list.size == originalList.size)
-        return list
+        return nodes.map { it.value }
     }
 
     private fun getGroveCoors(list: List<Long>): Long {
@@ -61,6 +51,31 @@ class Day20(inputName: String) {
         return listOf(1000, 2000, 3000).sumOf {
             list[(zeroIndex + it).rem(list.size)]
         }
+    }
+
+    private class Node private constructor(
+        val value: Long,
+        var mixedCount: Int,
+        private var _nextNode: Node? = null,
+    ) {
+        companion object {
+            fun listFromLongs(list: List<Long>): List<Node> {
+                val nodes = list.map {
+                    Node(
+                        value = it,
+                        mixedCount = 0,
+                    )
+                }
+                nodes.forEachIndexed { index, node ->
+                    node._nextNode = nodes[(index + 1).rem(nodes.size)]
+                }
+                return nodes
+            }
+        }
+
+        val nextNode: Node get() = _nextNode!!
+
+        override fun toString(): String = "Node(value=$value, mixedCount=$mixedCount, nextNodeValue=${nextNode.value})"
     }
 }
 
