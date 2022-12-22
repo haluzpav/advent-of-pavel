c19c0b76b2badd97e552f27f611c9e739021dfb4
haluzpav <24211726+haluzpav@users.noreply.github.com>
2022-12-23 00:03:44 +0100

    Cleanup

--- src/main/kotlin/Day21.kt
+++ src/main/kotlin/Day21.kt
@@ -1,18 +1,20 @@
 class Day21(inputName: String) {
     private val input: Sequence<String> = readInput(inputName)
 
+    companion object {
+        private const val rootMonkey = "root"
+        private const val humnMonkey = "humn"
+    }
+
     fun part1(): Long {
         val monkeyYells = parseMonkeys().toMutableMap()
-        return yell(monkeyYells, "root")
+        return yell(monkeyYells, rootMonkey, skipHumn = false)!!
     }
 
     fun part2(): Long {
         val monkeyYells = parseMonkeys().toMutableMap()
-        println(countMonkeyYells(monkeyYells, "root"))
-        println(evaluateNonHumn(monkeyYells, "root"))
-        println(countMonkeyYells(monkeyYells, "root"))
-        println(printMonkeyYells(monkeyYells, "root"))
-        val (number, monkeyCoveringHumn) = splitMonkeyToNumberAndMath(monkeyYells, "root")
+        yell(monkeyYells, rootMonkey, skipHumn = true)
+        val (number, monkeyCoveringHumn) = splitMonkeyToNumberAndMath(monkeyYells, rootMonkey)
         return digHumnOutOfMonkeys(monkeyYells, monkeyCoveringHumn, number)
     }
 
@@ -31,33 +33,12 @@ class Day21(inputName: String) {
         name to yell
     }.toMap()
 
-    private fun yell(monkeyYells: MutableMap<String, MonkeyYell>, yellingMonkey: String): Long =
-        when (val yell = monkeyYells[yellingMonkey]) {
-            is MonkeyYell.Math -> when (yell.operation) {
-                '+' -> yell(monkeyYells, yell.monkey1) + yell(monkeyYells, yell.monkey2)
-                '-' -> yell(monkeyYells, yell.monkey1) - yell(monkeyYells, yell.monkey2)
-                '*' -> yell(monkeyYells, yell.monkey1) * yell(monkeyYells, yell.monkey2)
-                '/' -> yell(monkeyYells, yell.monkey1) / yell(monkeyYells, yell.monkey2)
-                else -> error("unknown yell ${yell.operation} for monkey $yellingMonkey")
-            }.also { monkeyYells[yellingMonkey] = MonkeyYell.Number(it) }
-            is MonkeyYell.Number -> yell.number
-            null -> error("unknown yell for monkey $yellingMonkey")
-        }
-
-    private fun countMonkeyYells(monkeyYells: Map<String, MonkeyYell>, yellingMonkey: String): Int =
-        when (val yell = monkeyYells[yellingMonkey]) {
-            is MonkeyYell.Math -> countMonkeyYells(monkeyYells, yell.monkey1) +
-                countMonkeyYells(monkeyYells, yell.monkey2) + 1
-            is MonkeyYell.Number -> 1
-            null -> error("unknown yell for monkey $yellingMonkey")
-        }
-
-    private fun evaluateNonHumn(monkeyYells: MutableMap<String, MonkeyYell>, yellingMonkey: String): Long? {
-        if (yellingMonkey == "humn") return null
+    private fun yell(monkeyYells: MutableMap<String, MonkeyYell>, yellingMonkey: String, skipHumn: Boolean): Long? {
+        if (skipHumn && yellingMonkey == humnMonkey) return null
         return when (val yell = monkeyYells[yellingMonkey]) {
             is MonkeyYell.Math -> {
-                val evaluatedMonkey1 = evaluateNonHumn(monkeyYells, yell.monkey1)
-                val evaluatedMonkey2 = evaluateNonHumn(monkeyYells, yell.monkey2)
+                val evaluatedMonkey1 = yell(monkeyYells, yell.monkey1, skipHumn)
+                val evaluatedMonkey2 = yell(monkeyYells, yell.monkey2, skipHumn)
                 if (evaluatedMonkey1 != null && evaluatedMonkey2 != null) {
                     when (yell.operation) {
                         '+' -> evaluatedMonkey1 + evaluatedMonkey2
@@ -75,24 +56,6 @@ class Day21(inputName: String) {
         }
     }
 
-    private fun printMonkeyYells(monkeyYells: Map<String, MonkeyYell>, yellingMonkey: String): String {
-        val yell = monkeyYells[yellingMonkey]
-        if (yellingMonkey == "humn") return "x"
-        return when (yell) {
-            is MonkeyYell.Math -> {
-                val monkey1Yell = printMonkeyYells(monkeyYells, yell.monkey1)
-                val monkey2Yell = printMonkeyYells(monkeyYells, yell.monkey2)
-                if (yellingMonkey == "root") {
-                    "$monkey1Yell = $monkey2Yell"
-                } else {
-                    "($monkey1Yell${yell.operation}$monkey2Yell)"
-                }
-            }
-            is MonkeyYell.Number -> yell.number.toString()
-            null -> error("unknown yell for monkey $yellingMonkey")
-        }
-    }
-
     private fun splitMonkeyToNumberAndMath(
         monkeyYells: MutableMap<String, MonkeyYell>,
         yellingMonkey: String,
@@ -101,7 +64,7 @@ class Day21(inputName: String) {
         val side1 = monkeyYells[monkey.monkey1]!!
         val side2 = monkeyYells[monkey.monkey2]!!
         return when {
-            monkey.monkey1 == "humn" || side1 is MonkeyYell.Math -> Triple((side2 as MonkeyYell.Number).number, monkey.monkey1, false)
+            monkey.monkey1 == humnMonkey || side1 is MonkeyYell.Math -> Triple((side2 as MonkeyYell.Number).number, monkey.monkey1, false)
             side1 is MonkeyYell.Number -> Triple(side1.number, monkey.monkey2, true)
             else -> error("Can't split monkey $yellingMonkey")
         }
@@ -112,7 +75,7 @@ class Day21(inputName: String) {
         monkeyCoveringHumn: String,
         number: Long,
     ): Long {
-        if (monkeyCoveringHumn == "humn") return number
+        if (monkeyCoveringHumn == humnMonkey) return number
         val monkey = monkeyYells[monkeyCoveringHumn] as MonkeyYell.Math
         val (yellNumber, newCoveringMonkey, isNumberOnLeft) = splitMonkeyToNumberAndMath(monkeyYells, monkeyCoveringHumn)
         val newNumber = when (monkey.operation) {
