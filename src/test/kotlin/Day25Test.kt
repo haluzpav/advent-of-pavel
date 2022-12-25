0f7b5865d902574a881e36460e7e801cabe3291b
haluzpav <24211726+haluzpav@users.noreply.github.com>
2022-12-25 13:56:30 +0100

    Day 25 🦌⭐️🧉

---
+++ src/test/kotlin/Day25Test.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day25Test {
+    private val task = Day25("Day25_test")
+
+    @Test
+    fun testPart1() {
+        assertEquals("2=-1=0", task.part1())
+    }
+}
