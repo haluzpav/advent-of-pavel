00cee1ec408890851538282bf465c0ad3b65d09f
haluzpav <24211726+haluzpav@users.noreply.github.com>
2023-11-29 13:21:13 +0100

    Parse single example data

--- scripts/new_day.main.kts
+++ scripts/new_day.main.kts
@@ -1,5 +1,6 @@
 #!/usr/bin/env kotlin
 
+import java.io.InputStream
 import java.nio.file.Path
 import java.nio.file.StandardOpenOption
 import kotlin.io.path.Path
@@ -7,9 +8,10 @@ import kotlin.io.path.createParentDirectories
 import kotlin.io.path.isDirectory
 import kotlin.io.path.notExists
 import kotlin.io.path.outputStream
+import kotlin.io.path.writeLines
 import kotlin.io.path.writeText
 
-// TODO parse example test data
+// TODO handle more than one example test data in a day
 // TODO also update createDayCommon somehow...
 
 fun String.isPositiveInt(): Boolean = toIntOrNull().let { it != null && it > 0 }
@@ -49,7 +51,7 @@ fun createKotlinSrcFileContent(year: String, day: String): String = """
 
 """.trimIndent()
 
-fun createKotlinTestFileContent(year: String, day: String): String = """
+fun createKotlinTestFileContent(year: String, day: String, answerA: String?, answerB: String?): String = """
     package cz.veleto.aoc.year$year
 
     import cz.veleto.aoc.core.AocDay
@@ -61,16 +63,16 @@ fun createKotlinTestFileContent(year: String, day: String): String = """
 
         @Test
         fun testPart1() {
-            assertEquals("TODO", task.part1())
+            assertEquals("%s", task.part1())
         }
 
         @Test
         fun testPart2() {
-            assertEquals("TODO", task.part2())
+            assertEquals("%s", task.part2())
         }
     }
 
-""".trimIndent()
+""".trimIndent().format(answerA ?: "TODO", answerB ?: "TODO")
 
 fun Path.createFile(text: String) {
     writeText(
@@ -80,7 +82,13 @@ fun Path.createFile(text: String) {
     )
 }
 
-fun createKotlinFiles(modulePath: Path, year: String, day: String): List<Path> {
+fun createKotlinFiles(
+    modulePath: Path,
+    year: String,
+    day: String,
+    answerA: String?,
+    answerB: String?,
+): List<Path> {
     val packagePath = Path("cz", "veleto", "aoc", "year$year")
 
     fun createKotlinFilePath(type: String, fileSuffix: String = ""): Path =
@@ -93,22 +101,23 @@ fun createKotlinFiles(modulePath: Path, year: String, day: String): List<Path> {
     filePaths.forEach { it.createParentDirectories() }
 
     srcFile.createFile(createKotlinSrcFileContent(year, day))
-    testFile.createFile(createKotlinTestFileContent(year, day))
+    testFile.createFile(createKotlinTestFileContent(year, day, answerA, answerB))
 
     return filePaths
 }
 
 fun createInputFilePath(modulePath: Path, day: String, fileSuffix: String = ""): Path =
-    modulePath.resolve("inputs").resolve("Day$day$fileSuffix.txt")
+    modulePath.resolve("inputs").resolve("Day$day$fileSuffix.txt").also {
+        check(it.notExists()) { "Input file already exists" }
+    }
 
-fun Path.fetchInput(year: String, day: String, commandSuffix: String? = null) {
-    check(notExists()) { "Input file already exists" }
+fun fetchInput(year: String, day: String, commandSuffix: String? = null): InputStream {
     val aocd: Process = listOfNotNull("aocd", year, day, commandSuffix).toTypedArray().awaitProcess()
     if (aocd.exitValue() != 0) {
         aocd.inputStream.copyTo(System.err)
         error("AOCD failed")
     }
-    aocd.inputStream.copyTo(outputStream())
+    return aocd.inputStream
 }
 
 fun Array<String>.awaitProcess(): Process = Runtime.getRuntime().exec(this).apply {
@@ -116,17 +125,49 @@ fun Array<String>.awaitProcess(): Process = Runtime.getRuntime().exec(this).appl
     waitFor()
 }
 
-fun createInputFiles(modulePath: Path, year: String, day: String): List<Path> {
+fun createInputFiles(modulePath: Path, year: String, day: String): Triple<List<Path>, String?, String?> {
     val exampleInputFile = createInputFilePath(modulePath, day, fileSuffix = "_test")
     val seriousInputFile = createInputFilePath(modulePath, day)
 
-    val filePaths = listOf(exampleInputFile, seriousInputFile)
-    filePaths.forEach { it.createParentDirectories() }
+    val inputFiles = listOf(exampleInputFile, seriousInputFile)
+    inputFiles.forEach { it.createParentDirectories() }
 
-    exampleInputFile.fetchInput(year, day, commandSuffix = "-e")
-    seriousInputFile.fetchInput(year, day)
+    val (answerA, answerB) = fetchExampleInput(exampleInputFile, year, day)
+    fetchInput(year, day).copyTo(seriousInputFile.outputStream())
 
-    return filePaths
+    return Triple(inputFiles, answerA, answerB)
+}
+
+fun fetchExampleInput(targetPath: Path, year: String, day: String): Pair<String?, String?> {
+    val lines = fetchInput(year, day, commandSuffix = "-e").reader().readLines()
+    check("/${year}/day/${day.toInt()} " in lines[1]) { "Example input isn't for the requested year and day" }
+
+    val hasSingleData = " 1/1 " in lines[2]
+    return if (hasSingleData) {
+        parseSingleData(targetPath, lines)
+    } else {
+        targetPath.writeLines(lines)
+        null to null
+    }
+}
+
+fun parseSingleData(targetPath: Path, lines: List<String>): Pair<String?, String?> {
+    val separatorStart = "---"
+    val answerARegex = Regex("^answer_a: (.+)$")
+    val answerBRegex = Regex("^answer_b: (.+)$")
+
+    val answerAIndex = lines.indexOfFirst { it.matches(answerARegex) }
+    check(answerAIndex - 1 > 2) { "Example data don't match expected format [0]" }
+    check(lines[answerAIndex - 1].startsWith(separatorStart)) { "Example data don't match expected format [1]" }
+    check(lines[answerAIndex + 1].matches(answerBRegex)) { "Example data don't match expected format [2]" }
+    check(lines[answerAIndex + 2].startsWith(separatorStart)) { "Example data don't match expected format [3]" }
+
+    targetPath.writeLines(lines.subList(3, answerAIndex - 1))
+
+    val (answerA) = answerARegex.matchEntire(lines[answerAIndex])!!.destructured
+    val (answerB) = answerBRegex.matchEntire(lines[answerAIndex + 1])!!.destructured
+
+    return answerA to answerB
 }
 
 fun addFilesToGit(filePaths: List<Path>) {
@@ -140,11 +181,11 @@ fun main() {
     val (year, day) = parseArgs()
     val modulePath = createModulePath(year)
 
-    println("Creating Kotlin files...")
-    val kotlinFiles = createKotlinFiles(modulePath, year, day)
-
     println("Fetching input data...")
-    val inputFiles = createInputFiles(modulePath, year, day)
+    val (inputFiles, answerA, answerB) = createInputFiles(modulePath, year, day)
+
+    println("Creating Kotlin files...")
+    val kotlinFiles = createKotlinFiles(modulePath, year, day, answerA, answerB)
 
     val createdFiles = kotlinFiles + inputFiles
 
