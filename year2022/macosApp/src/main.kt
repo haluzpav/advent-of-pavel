8b6a792d0f5f9d2c444898a3b6cb4598a7f0d6c4
haluzpav <24211726+haluzpav@users.noreply.github.com>
2023-11-29 12:17:25 +0100

    Avoid duplicate runs in mains

--- year2022/macosApp/src/main.kt
+++ year2022/macosApp/src/main.kt
@@ -1,8 +1,7 @@
 import cz.veleto.aoc.core.runAllDays
-import cz.veleto.aoc.core.runSingleDay
 import cz.veleto.aoc.year2022.createDayCommon
 
 fun main() {
-    runSingleDay(25, ::createDayCommon)
+    // runSingleDay(25, ::createDayCommon)
     runAllDays(::createDayCommon, 19, 24)
 }
--- year2022/src@jvm/main.kt
+++ year2022/src@jvm/main.kt
@@ -1,8 +1,7 @@
 import cz.veleto.aoc.core.runAllDays
-import cz.veleto.aoc.core.runSingleDay
 import cz.veleto.aoc.year2022.createDayJvm
 
 fun main() {
-    runSingleDay(25, ::createDayJvm)
+    // runSingleDay(25, ::createDayJvm)
     runAllDays(::createDayJvm)
 }
