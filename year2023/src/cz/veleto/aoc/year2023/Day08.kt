984d79424885c9f35b999a86aa98f1623b235f93
haluzpav <24211726+haluzpav@users.noreply.github.com>
2023-12-08 12:37:53 +0100

    Day 8, part 2 [running for 40 mins and still not done 😳]

--- year2023/src/cz/veleto/aoc/year2023/Day08.kt
+++ year2023/src/cz/veleto/aoc/year2023/Day08.kt
@@ -4,31 +4,69 @@ import cz.veleto.aoc.core.AocDay
 
 class Day08(config: Config) : AocDay(config) {
 
-    private val nodeRegex = Regex("""^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$""")
+    private val nodeRegex = Regex("""^(\w{3}) = \((\w{3}), (\w{3})\)$""")
 
     override fun part1(): String {
         val (instructions, nodes) = cachedInput.parse()
-        val startNode = "AAA"
-        val endNode = "ZZZ"
-        check(startNode in nodes)
-        check(endNode in nodes)
+        val startNode = "AAA".also { check(it in nodes) }
+        val endNode = "ZZZ".also { check(it in nodes) }
+        val startState = State1(
+            currentNode = nodes[startNode]!!,
+            stepsTaken = 0,
+        )
+
+        fun State1.isEndState(): Boolean = currentNode.name == endNode
         return instructions
-            .runningFold(State1(nodes[startNode]!!, 0)) { state, instruction ->
-                val newNode = when (instruction) {
-                    'L' -> state.currentNode.left
-                    'R' -> state.currentNode.right
-                    else -> error("undefined instruction $instruction")
-                }
-                State1(nodes[newNode]!!, state.stepsTaken + 1)
+            .runningFold(startState) { state, instruction ->
+                State1(
+                    currentNode = state.currentNode.navigateToNextNode(nodes, instruction),
+                    stepsTaken = state.stepsTaken + 1,
+                )
             }
-            .first { state -> state.currentNode.name == endNode }
+            .first(State1::isEndState)
             .stepsTaken
             .toString()
     }
 
     override fun part2(): String {
-        // TODO
-        return ""
+        val (instructions, nodes) = cachedInput.parse()
+        val startState = State2(
+            currentNodes = nodes.values.filter { it.name.endsWith('A') }.also { check(it.isNotEmpty()) },
+            stepsTaken = 0,
+        )
+
+        fun Node.isEndNode(): Boolean = name.endsWith('Z')
+        fun State2.isEndState(): Boolean = currentNodes.all(Node::isEndNode)
+
+        if (config.log) println("Starting at ${startState.nodeNames}")
+        return instructions
+            .runningFold(startState) { state, instruction ->
+                State2(
+                    currentNodes = state.currentNodes.map { it.navigateToNextNode(nodes, instruction) },
+                    stepsTaken = state.stepsTaken + 1,
+                ).also { newState ->
+                    if (config.log) {
+                        if (newState.stepsTaken % 10_000_000 == 0L) {
+                            println("Step ${newState.stepsTaken}, navigating to ${newState.nodeNames}")
+                        }
+                        if (newState.currentNodes.count { it.isEndNode() } >= 4) {
+                            println("Step ${newState.stepsTaken}, found 4 or more nodes in end-state ${newState.nodeNames}")
+                        }
+                    }
+                }
+            }
+            .first(State2::isEndState)
+            .stepsTaken
+            .toString()
+    }
+
+    private fun Node.navigateToNextNode(nodes: Map<String, Node>, instruction: Char): Node {
+        val newNode = when (instruction) {
+            'L' -> left
+            'R' -> right
+            else -> error("undefined instruction $instruction")
+        }
+        return nodes[newNode]!!
     }
 
     private fun List<String>.parse(): Pair<Sequence<Char>, Map<String, Node>> {
@@ -58,6 +96,9 @@ class Day08(config: Config) : AocDay(config) {
 
     data class State2(
         val currentNodes: List<Node>,
-        val stepsTaken: Int,
-    )
+        val stepsTaken: Long,
+    ) {
+        val nodeNames: List<String>
+            get() = currentNodes.map { it.name }
+    }
 }
