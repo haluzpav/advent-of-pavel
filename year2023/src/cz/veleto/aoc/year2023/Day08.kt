b97e49e68740163d9b2c6b00a4d9eb3d84f71d21
haluzpav <24211726+haluzpav@users.noreply.github.com>
2023-12-08 11:27:56 +0100

    Day 8, part 2 [prepare]

--- year2023/src/cz/veleto/aoc/year2023/Day08.kt
+++ year2023/src/cz/veleto/aoc/year2023/Day08.kt
@@ -5,24 +5,21 @@ import cz.veleto.aoc.core.AocDay
 class Day08(config: Config) : AocDay(config) {
 
     private val nodeRegex = Regex("""^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$""")
-    private val startNode = "AAA"
-    private val endNode = "ZZZ"
 
     override fun part1(): String {
-        val instructions = sequence {
-            while (true) yieldAll(cachedInput[0].asSequence())
-        }
-        val nodes = cachedInput.drop(2).map { it.parseNode() }.associateBy(Node::name)
+        val (instructions, nodes) = cachedInput.parse()
+        val startNode = "AAA"
+        val endNode = "ZZZ"
         check(startNode in nodes)
         check(endNode in nodes)
         return instructions
-            .runningFold(State(nodes[startNode]!!, 0)) { state, instruction ->
+            .runningFold(State1(nodes[startNode]!!, 0)) { state, instruction ->
                 val newNode = when (instruction) {
                     'L' -> state.currentNode.left
                     'R' -> state.currentNode.right
                     else -> error("undefined instruction $instruction")
                 }
-                State(nodes[newNode]!!, state.stepsTaken + 1)
+                State1(nodes[newNode]!!, state.stepsTaken + 1)
             }
             .first { state -> state.currentNode.name == endNode }
             .stepsTaken
@@ -32,6 +29,15 @@ class Day08(config: Config) : AocDay(config) {
     override fun part2(): String {
         // TODO
         return ""
+    }
+
+    private fun List<String>.parse(): Pair<Sequence<Char>, Map<String, Node>> {
+        val instructions = this[0].toList()
+        val repeatingInstructions = sequence {
+            while (true) yieldAll(instructions)
+        }
+        val nodes = drop(2).map { it.parseNode() }.associateBy(Node::name)
+        return repeatingInstructions to nodes
     }
 
     private fun String.parseNode(): Node {
@@ -45,8 +51,13 @@ class Day08(config: Config) : AocDay(config) {
         val right: String,
     )
 
-    data class State(
+    data class State1(
         val currentNode: Node,
+        val stepsTaken: Int,
+    )
+
+    data class State2(
+        val currentNodes: List<Node>,
         val stepsTaken: Int,
     )
 }
--- year2023/test/cz/veleto/aoc/year2023/Day08Test.kt
+++ year2023/test/cz/veleto/aoc/year2023/Day08Test.kt
@@ -8,7 +8,13 @@ class Day08Test {
 
     @Test
     fun testPart1() {
-        val task = Day08(AocDay.Config("Day08_test"))
+        val task = Day08(AocDay.Config("Day08_test_1"))
         assertEquals("2", task.part1())
+    }
+
+    @Test
+    fun testPart2() {
+        val task = Day08(AocDay.Config("Day08_test_2"))
+        assertEquals("6", task.part2())
     }
 }
