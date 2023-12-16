afd06e10718289eb739a952e88a362532b0bed99
haluzpav <24211726+haluzpav@users.noreply.github.com>
2023-12-16 11:31:21 +0100

    Day 16, part 2 [1m brute-force, yolo]

--- year2023/src/cz/veleto/aoc/year2023/Day16.kt
+++ year2023/src/cz/veleto/aoc/year2023/Day16.kt
@@ -7,29 +7,38 @@ import cz.veleto.aoc.core.plus
 
 class Day16(config: Config) : AocDay(config) {
 
-    override fun part1(): String {
-        return generateSequence { }
-            .runningFold(State(beams = setOf(Pos(0, 0) to Direction.Right))) { state, _ ->
-                val newBeams = state.beams.flatMap { step(it, cachedInput) }
-                State(
-                    beams = newBeams.toSet(),
-                    energized = state.energized + newBeams,
-                )
+    override fun part1(): String = countEnergizedTiles(startBeam = Pos(0, 0) to Direction.Right).toString()
+
+    override fun part2(): String {
+        val lineIndices = cachedInput.indices
+        val columnIndices = cachedInput[0].indices
+        val startBeams = columnIndices.map { Pos(0, it) to Direction.Down } +
+            lineIndices.map { Pos(it, columnIndices.last) to Direction.Left } +
+            columnIndices.map { Pos(lineIndices.first, it) to Direction.Up } +
+            lineIndices.map { Pos(it, 0) to Direction.Right }
+        return startBeams
+            .maxOf { startBeam ->
+                if (config.log) println("Energizing tiles from $startBeam")
+                countEnergizedTiles(startBeam = startBeam)
             }
-            .zipWithNext()
-            .first { (old, new) -> old.energized == new.energized }
-            .first
-            .energized
-            .map { it.first }
-            .toSet()
-            .size
             .toString()
     }
 
-    override fun part2(): String {
-        // TODO
-        return ""
-    }
+    private fun countEnergizedTiles(startBeam: Pair<Pos, Direction>): Int = generateSequence { }
+        .runningFold(State(beams = setOf(startBeam))) { state, _ ->
+            val newBeams = state.beams.flatMap { step(it, cachedInput) }
+            State(
+                beams = newBeams.toSet(),
+                energized = state.energized + newBeams,
+            )
+        }
+        .zipWithNext()
+        .first { (old, new) -> old.energized == new.energized }
+        .first
+        .energized
+        .map { it.first }
+        .toSet()
+        .size
 
     private fun step(beam: Pair<Pos, Direction>, map: List<String>): List<Pair<Pos, Direction>> {
         val (beamPos, beamDirection) = beam
