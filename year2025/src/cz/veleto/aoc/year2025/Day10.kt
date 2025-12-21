97b36a4c899a68e106a3efb5a9259dbbdbdc71e1
haluzpav <24211726+haluzpav@users.noreply.github.com>
2025-12-21 20:33:37 +0100

    Basic but optimized DFS, still not enough, ugh

--- year2025/src/cz/veleto/aoc/year2025/Day10.kt
+++ year2025/src/cz/veleto/aoc/year2025/Day10.kt
@@ -1,6 +1,7 @@
 package cz.veleto.aoc.year2025
 
 import cz.veleto.aoc.core.AocDay
+import kotlin.math.pow
 
 class Day10(override val config: Year2025Config) : AocDay(config) {
 
@@ -22,7 +23,8 @@ class Day10(override val config: Year2025Config) : AocDay(config) {
                 targetJoltages = machine.targetJoltages,
                 buttonTransitions = machine.buttonTransitions,
             ).also {
-                if (config.log) println("\tmin transitions to match joltage = $it")
+                if (config.log) println(" min transitions to match joltage = $it")
+                TODO("one found, ta-da")
             }
         }
         .sum()
@@ -84,8 +86,79 @@ class Day10(override val config: Year2025Config) : AocDay(config) {
     private fun getMinTransitionCountToMatchJoltage(
         targetJoltages: List<Int>,
         buttonTransitions: List<Set<Int>>,
-    ): Int {
-        TODO()
+    ): Int = dfs(
+        joltages = List(targetJoltages.size) { 0 },
+        targetJoltages = targetJoltages,
+        buttonTransitions = buttonTransitions,
+    )!!
+
+    // TODO track+log discarded hits
+    private fun dfs(
+        joltages: List<Int>,
+        targetJoltages: List<Int>,
+        buttonTransitions: List<Set<Int>>,
+        discardedJoltages: MutableSet<List<Int>> = mutableSetOf(),
+        depth: Int = 0,
+    ): Int? {
+        val log = config.verboseLog || config.log && discardedJoltages.size % 100_000 == 0
+        if (log) println("dfs joltages = $joltages, discarded = ${discardedJoltages.size}".prependIndent(depth + 1))
+        return when {
+            joltages in discardedJoltages -> null.also {
+                if (config.verboseLog) println("-> already discarded".prependIndent(depth + 2))
+            }
+
+            joltages == targetJoltages -> depth.also {
+                if (config.verboseLog) println("-> reached goal".prependIndent(depth + 2))
+            }
+
+            joltages.zip(targetJoltages) { a, b -> a > b }.any { it } -> null.also { discardedJoltages.add(joltages) }
+                .also {
+                    if (config.verboseLog) println("-> beyond goal, discarding".prependIndent(depth + 2))
+                }
+
+            else -> buttonTransitions
+                .reorder(remainingJoltages = targetJoltages.zip(joltages, Int::minus), depth = depth)
+                .asSequence()
+                .map { buttonTransition ->
+                    dfs(
+                        joltages = joltages.asJoltagesWithAppliedButton(buttonTransition),
+                        targetJoltages = targetJoltages,
+                        buttonTransitions = buttonTransitions,
+                        discardedJoltages = discardedJoltages,
+                        depth = depth + 1,
+                    )
+                }
+                .filterNotNull()
+                .firstOrNull()
+                .also {
+                    if (it == null) discardedJoltages.add(joltages)
+                    if (config.verboseLog) println(
+                        "-> ${if (it == null) "child discarded" else "child reached goal at $it"}"
+                            .prependIndent(depth + 2)
+                    )
+                }
+        }
+    }
+
+    private fun String.prependIndent(level: Int = 1, element: String = " "): String =
+        prependIndent(element.repeat(level))
+
+    // TODO also prioritize buttons which affect joltages with least buttons
+    // TODO group remaining joltages so the above can take effect
+    private fun List<Set<Int>>.reorder(remainingJoltages: List<Int>, depth: Int = 0): List<Set<Int>> {
+        val priorityJoltagesIndices = remainingJoltages.withIndex().sortedBy { it.value }.map { it.index }
+        return sortedByDescending { buttonTransition ->
+            buttonTransition.sumOf { 2.0.pow(priorityJoltagesIndices.indexOf(it)) }
+        }.also {
+            if (config.verboseLog) println(
+                """
+                    reorder:
+                        remainingJoltages = $remainingJoltages
+                        previousButtonTransitions = $this
+                        newButtonTransitions      = $it
+                """.replaceIndent(" ".repeat(depth + 1))
+            )
+        }
     }
 
     private fun List<Int>.asJoltagesWithAppliedButton(transitions: Set<Int>): List<Int> = this
